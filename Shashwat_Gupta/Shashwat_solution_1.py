7843ff84bcf188ad15f99817ef1874ade2d41c63
Shashwat <stgupta9935@gmail.com>
2021-10-27 01:16:54 +0530

    Solution of Basic program 1 (The Palindrome Program) by Shashwat Gupta

---
+++ Shashwat_Gupta/Shashwat_solution_1.py
@@ -0,0 +1,11 @@
+#Answer is in python language
+str = input("Enter the string: ")
+if str == str[::-1]:
+    print("String is palindrome")
+else:
+    print("String is not palindrome")#Answer is in python language
+str = input("Enter the string: ")
+if str == str[::-1]:
+    print("String is palindrome")
+else:
+    print("String is not palindrome")
